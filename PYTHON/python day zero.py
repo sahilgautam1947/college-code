809d1f7453bd36270cdac6b49ad703613cd44dce
sahilgautam1947 <sahilgautam1947@gmail.com>
2022-09-01 18:32:09 +0530

    this was my first day at class of python

---
+++ PYTHON/python day zero.py
@@ -0,0 +1,23 @@
+x=10
+y=20
+#bodmas error
+z=x+y/2
+print(z)
+# hi there please mend your logic and maths and bracket placement
+
+print((x+y)/2)
+#line 8 is an example of a bad code
+#declare (x+y)/2 as another variable say 'm' using assignment operator
+#variables are case sensitive
+# 0-9
+# abc lower case
+# ABC upper case
+# start with either underscore or alphabets but not with letters
+# mixing is allowed
+
+ab123= 20
+print(ab123)
+
+class=20
+print(class)
+
