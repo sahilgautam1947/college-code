b826838ac0661a5c156fc78c3d4d68d24d2c591c
sahilgautam1947 <sahilgautam1947@gmail.com>
2022-09-03 23:17:02 +0530

    ok

---
+++ my notes/notesday1.py
@@ -0,0 +1,41 @@
+# the software developement process is a lot more than just programming as 
+# it takes a lot to write a good code 
+
+# first of all the need for programming is analysed that may be customer project or some other requirement 
+# the next thing that is done is to analyse what the programm will do 
+# the next step is design that is how the programm will do the task required 
+# then comes the coding step in which the code is written 
+# then it is the integration part in which we combine the smaller parts to make a smoothly working programm which is not easy 
+# and the most important part is maintainence which is usually the most expensive one 
+
+
+# sometinmtimes bugs are found in the programm so as to solve them we may have to go to the previous steps also 
+
+
+
+# Q List four phases of the software development process, and explain what they accomplish
+
+# ans 
+
+#     requirement 
+#     what the programm will do 
+#     how the programm will do 
+#     coding part 
+#     integration 
+#     implimentation
+#     maintainence 
+
+
+# Q Jack says that he will not bother with analysis and design but proceed directly to coding his programs.
+#  Why is that not a good idea?
+
+#  ans 
+
+#      this is not a good idea as to write a good code it is the basic requirement that thorough analysis is done 
+#      so as to prevent bugs or unstabilirty issues in later stages 
+#      also if some thing like a blue print or a pseudocode is made in advanced then it then becomes really easy 
+#      to code when the logic has been previously build and the planning has been done 
+
+# new shotcut discovered 
+# if you press ctrl+shift plus any arrow keys then a word gets selected and 
+# that combination of words is highlighted in the whole code 
