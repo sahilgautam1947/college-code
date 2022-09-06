afafbdf8cd0fb9eb0b85fbf2f37a0b3fa90e907b
sahilgautam1947 <sahilgautam1947@gmail.com>
2022-09-06 18:51:36 +0530

    thats ok

---
+++ PYTHON/pythonday02.py
@@ -0,0 +1,73 @@
+#principal amount
+p = int(input("what is the principle amount: "))
+# rate of interest 
+r=float(input("what is the rate of interest: "))
+
+y= float(input("what is the time period: "))
+
+final=p*y + y*r*p*0.01
+
+print(final)
+# dry running the code is to run the code manually
+# like the duck talking
+
+
+
+
+
+a = "joe warren is"
+b= " 52 "
+c= "years old"
+d= a+b+c
+print(d)
+# so here we either have to convert integer to string data type by putting it in either single quotes or double quotes
+# single quotes are also characters''
+
+
+
+
+
+
+a="my name is"
+b="joe"
+c="warren"
+s=' '
+ans= a+s+b+s+c
+print(ans)
+# do not copy and paste your code
+# these were the examples of concatination
+
+
+
+# 2**2==2^2
+
+
+
+
+# how is it possible to get an input from the user
+# so here we use a function called input function
+# input by default takes a string value but to take other values you have to do conversion from string to anothe data type that is needed
+
+length=int(input("the length of the rectangle is:"))
+breadth=int(input("the breadth of the rectangle is:"))
+area=length*breadth
+print ("the area of the rectangle is: ",area )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
