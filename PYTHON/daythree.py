8030692f6157aa038979e2a7091652c827bfcfd2
sahilgautam1947 <sahilgautam1947@gmail.com>
2022-09-08 18:56:47 +0530

    ok

---
+++ PYTHON/daythree.py
@@ -0,0 +1,95 @@
+a=100
+b=100
+print(a is b)
+
+id(a)==id(b)
+print(id(a))
+print(id(b))
+
+c="4654fg65465f4g"
+d="4654fg65465f4g"
+id(c)==id(d)
+
+
+
+
+g=int(input("what is the first number: "))
+
+h=int(input("what is the second number: "))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print(g+h)
+print(g-h)
+print(g*h)
+print(g**h)
+print(g%h)
+print(g and h)
+print(g or h)
+print(g>h)
+print(g<h)
+print(g!=h)
+# this is the solution
+# >>> g=int(input("what is the first number: "))
+# what is the first number: 100
+# >>> h=int(input("what is the second number: "))
+# what is the second number: 2
+# >>> print(g+h)
+# 102
+# >>> print(g-h)
+# 98
+# >>> print(g*h)
+# 200
+# >>> print(g**h)
+# 10000
+# >>> print(g%h)
+# 0
+# >>> print(g and h)
+# 2
+# >>> print(g or h)
+# 100
+# >>> print(g>h)
+# True
+# >>> print(g<h)
+# False
+# >>> print(g!=h)
+# True
+
+
+
+a=2
+b=200
+print(~a)
+
+print(a&b)
+print(a|b)
+print(a^b)
+print(~a)
+print(a<<2)
+print(a>>2)
+
+
+# ##
+# so what is happening in the background is that it is converting the numbers to binary and then applying the operations (bitwise)
+# and after that it is adding the binary answers to give the answer
+# 
+# 
+# #
